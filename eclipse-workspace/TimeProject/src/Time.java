573df33549fe1e61640451410fe83c57d879eaae
dana-alsibai <alsibaid@sheridancollege.ca>
2022-03-14 16:20:40 -0400

    This is ICE6 First Commit

--- eclipse-workspace/TimeProject/src/Time.java
+++ eclipse-workspace/TimeProject/src/Time.java
@@ -32,7 +32,7 @@ public class Time
 		
 		int totalSeconds = getTotalSeconds("10:10:10");
 		System.out.println(" Total Seconds = " + totalSeconds );
-		
+		System.out.println("Github");	
 //		}
 //		catch(StringIndexOutOfBoundsException e)
 //		{
